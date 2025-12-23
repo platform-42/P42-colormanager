83dcaa0dca93b3a1a920d1ec276c1564a6e6c829
Diederick <diederick.de.buck@platform-42.com>
2025-12-23 08:46:15 +0100

    ffff

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "P42-colormanager",
     platforms: [
-        .iOS(.v16),
+        .iOS(.v17),
         .macOS(.v13)
     ],
     products: [
